1df8b6d9535c59d2fdce087041a6a159e43c7f97
neyjjia <nerdplanet333@gmail.com>
2026-06-28 12:43:19 -0300

    Update passwords.js

--- js/passwords.js
+++ js/passwords.js
@@ -30,10 +30,10 @@ const passwords = {
 const deniedMessages = {
   "lilith": "É TUDO CULPA DELA",
   "ivo": "Brincando com o tempo de novo?",
-  "yegor": "К чёрту государство!"
+  "yegor": "К чёрту государство!",
   "ordem": "Luz e ...",
   "dce": "Um sonho sobre dias melhores... ou não",
-  "armando": "Nunca subestime um homem com um cachorro no fim do mundo"
+  "armando": "Nunca subestime um homem com um cachorro no fim do mundo",
   "verissimo": "Os olhos se fecharam",
   "salvatore": "Colecionador de deuses",
   "william": "Qual dos dois?",
